4fee5948ab6aabd48ad17e6b62bc16acd8542470
tiantianquan <fenqiufen@gmail.com>
2015-04-19 22:00:03 +0800

    1

---
+++ Assets/CubeMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+//				this.rigidbody.AddForce(new Vector3(1000f,1000f,0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnCollisionEnter(Collision collision) 
+	{
+		this.rigidbody.AddForce(new Vector3(0f,1000f,0f));
+	}
+}
---
+++ Assets/MyTest1/BallScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	private Rigidbody rb;
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = 1;
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveInput = Input.GetAxis("Horizontal") ;
+		float moveInput2 = Input.GetAxis("Vertical") ;
+		float moveInput3 = 0.0f;
+		if(Input.GetKey(KeyCode.Space)){
+			moveInput3 = 10f;
+		}
+		rb.AddForce(new Vector3(moveInput,moveInput3,moveInput2)*speed);
+	}
+
+	void OnCollisionEnter(Collision collision) 
+	{
+//		this.rigidbody.AddForce(new Vector3(0f,100f,0f));
+	}
+}
